fdf318a3fba6c4412d0b2acd409ceab9fcadb72e
Phabiann-Ross De Carvalho <prdecarvalho96@outlook.com>
2021-08-12 10:02:44 +0200

    Initial

---
+++ ImplicitExplicitConversionShenanigans/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImplicitExplicitConversionShenanigans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
